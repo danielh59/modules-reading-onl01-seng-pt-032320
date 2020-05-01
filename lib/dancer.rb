03777c11b74353d80ecda90f9e0403c38656e4f2
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-01 01:41:44 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,14 +1,13 @@
-# require_relative './class_methods_module.rb'
-# require_relative './dance_module.rb'
-#
-# class Dancer
-#
-#   include Dance
-#   attr_accessor :name
-#
-# def initialize(name)
-#   @name = name
-#   extend MetaDancing
-# end
-#
-# end
+require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
+
+class Dancer
+  
+  include Dance
+  attr_accessor :name
+
+def initialize(name)
+  @name = name
+end
+
+end
